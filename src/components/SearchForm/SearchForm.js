e91a7f529a668391539c7655e8565e7b2ed412a2
Klaudia Stanczyk <klaudia.stanczyk92@gmail.com>
2022-05-17 23:45:11 +0200

    add button component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,12 @@
 import styles from './SearchForm.module.scss'
 import TextInput from '../TextInput/TextInput';
+import Button from '../Button/Button';
 
 const SearchForm = () => {
     return (
         <form className={styles.searchForm}>
             <TextInput placeholder="Search…" />
-            <TextInput placeholder="doopa" />
-            <button className={styles.button}>Search</button>
+            <Button text='Search'/>
         </form>
     );
 };
