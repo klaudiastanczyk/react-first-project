ec286c250a53db52d8e4457cf3106e0c5f7d3b94
Klaudia Stanczyk <klaudia.stanczyk92@gmail.com>
2022-06-08 23:02:26 +0200

    add action creators for addCard and searchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,15 +3,20 @@ import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
+import { updateSearchform } from '../../redux/store';
 
 const SearchForm = (props) => {
     const [userText, checkUserText] = useState('');
     const dispatch = useDispatch();
-    dispatch({ type: 'UPDATE_SEARCHSTRING', payload: userText })
+    const changeHandler = e => {
+        checkUserText(e.target.value);
+        dispatch(updateSearchform(userText));
+        }
 
     return (
         <form className={styles.searchForm}>
-            <TextInput placeholder={!props.children ?  'Search...' : props.children} value={userText} onChange={e => checkUserText(e.target.value)} />
+            <TextInput placeholder={!props.children ?  'Search...' : props.children} 
+                value={userText} onChange={ changeHandler } />
             <Button><span className="fa fa-search" /> Search</Button>
         </form>
     );
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,17 @@ import initialState from './initialState';
 import strContains from '../utils/strContains';
 
 //selectors
-
 export const getFilteredCards = ({ cards, searchString }, columnId) => cards
 .filter(card => card.columnId === columnId && strContains(card.title, searchString))
+
+export const getAllColumns = state => state.columns;
+
+// action creators
+export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
+
+export const addCard = payload => ({ type: 'ADD_CARD', payload });
+
+export const updateSearchform = payload => ({ type: 'UPDATE_SEARCHSTRING', payload });
 
 const reducer = (state, action) => {
   switch(action.type) {
